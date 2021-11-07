9d066fff456baaf88501a92aa6aeb5264682560a
Brayan Orlando Rivera Cepeda <44098760+BitanCepeda@users.noreply.github.com>
2021-11-07 10:56:26 -0500

    Update form.php

--- Php/form.php
+++ Php/form.php
@@ -28,10 +28,10 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = '';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = "v1270369@gmail.com";                     //SMTP username
-    $mail->Password   = 'v1270369skjhfsjiafda';                               //SMTP password
+    $mail->Username   = "";                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
